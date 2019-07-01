954464a89932998aee523af94c91d4e951062ce1
alk831 <adam54111@gmail.com>
2019-07-01 18:33:34 +0200

    refactor useCookies hook

---
+++ src/use-cookies/context.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useMemo, createContext } from "react";
+import { useCookies } from "./use-cookies";
+
+interface CookiesObject {
+  [key: string]: string | number | boolean | null | undefined
+}
+
+export const CookiesContext = createContext<[CookiesObject, () => {}]>(null);
+
+interface CookiesProviderProps {
+  children: ReactNode
+}
+export function CookiesProvider({ children }: CookiesProviderProps) {
+  const [cookies, setCookies] = useCookies();
+  const value = useMemo(() => [cookies, setCookies], [cookies]);
+
+  return (
+    <CookiesContext.Provider value={value}>
+      {children}
+    </CookiesContext.Provider>
+  );
+}
---
+++ src/use-cookies/index.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect, createContext, useMemo, ReactNode, useContext } from "react";
+
+/**
+ * Returns an array (useState) of object with parsed cookies and function for updating.
+ */
+export function useCookies<T>() {
+  const [cookies, setCookies] = useState<T>(() => parseCookies<T>(document.cookie));
+
+  useEffect(() => {
+    document.cookie = stringifyCookies(cookies);
+  }, [cookies]);
+
+  return [cookies, setCookies];
+}
+
+
+function parseCookies<T>(cookiesString: string): T {
+  return cookiesString
+    .split(';')
+    .reduce((parsed, cookie) => {
+      const [name, value] = cookie.split('=');
+      return {
+        ...parsed,
+        [name]: value
+      }
+    }, {} as T);
+}
+
+function stringifyCookies<T>(cookiesData: T): string {
+  return Object
+    .entries(cookiesData)
+    .map(([prop, value]) => `${prop}=${value}`)
+    .join(';');
+}
